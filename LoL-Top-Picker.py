74ed4f154b2e9fe3972b08ffb6126b348cb94832
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2022-10-13 08:35:49 -1000

    Update LoL-Top-Picker.py (#1)
    Love this! Here are some Pythonic changes I'm proposing:
    - Abstract the lists out of the random.choice() calls
    - Rename variables to be more semantic
    - Print dynamic string in f-string notation

--- LoL-Top-Picker.py
+++ LoL-Top-Picker.py
@@ -1,10 +1,14 @@
-#Who should I play?
+# Who should I play?
 
 from random import choice
 
-#answermid = choice([])
-#answer = choice(['Lee Sin', 'Olaf', 'Shyvana', 'Sejuani', 'Lillia', 'Kindred', "Kha'zix", "Talon" ])
-#print ("You should play", answerjg, "in the jungle")
+# mid_champs = []
+# mid_champ = choice(mid_champs)
 
-answer = choice([ 'Fiora', 'Jax', 'Renekton', 'Quinn', 'Sett', 'Riven', 'Kayle', 'Rengar', 'Volibear', 'Topturne', 'Vayne', 'Akshan' ])
-print ("You should play" answer, "in top lane!")
+# jg_champs = ['Lee Sin', 'Olaf', 'Shyvana', 'Sejuani', 'Lillia', 'Kindred', "Kha'zix", 'Talon']
+# jg_champ = choice(jg_champs)
+
+top_champs = ['Fiora', 'Jax', 'Renekton', 'Quinn', 'Sett', 'Riven', 'Kayle', 'Rengar', 'Volibear', 'Topturne', 'Vayne', 'Akshan']
+top_champ = choice(top_champs)
+
+print(f"You should play {top_champ} in top lane!")
